12c75d65def315ccaa22c7e84462ad29125cb91a
Shishir Kumar Singh <57223710+shishir474@users.noreply.github.com>
2023-04-28 16:24:29 +0530

    Update snowflake_assignment.sql

--- snowflake_assignment.sql
+++ snowflake_assignment.sql
@@ -1,66 +1,71 @@
--- create roles
-create or replace role admin;
-create or replace role developer;
-create or replace role pii;
+-- CREATE roles
+CREATE OR REPLACE ROLE admin;
+CREATE OR REPLACE ROLE developer;
+CREATE OR REPLACE ROLE pii;
 
--- assigning roles
-grant role admin
-to role accountadmin;
+-- assigning ROLEs
+GRANT ROLE admin
+TO ROLE accountadmin;
 
-grant role developer
-to role admin;
+GRANT ROLE developer
+TO ROLE admin;
 
-grant role pii
-to role accountadmin;
+GRANT ROLE pii
+TO ROLE accountadmin;
 
--- create warehouse
-use role accountadmin;
+-- CREATE warehouse
+USE ROLE accountadmin;
 
 
-create or replace warehouse assignment_wh
-with warehouse_type = 'Standard'
+CREATE OR REPLACE warehouse assignment_wh
+WITH warehouse_type = 'Standard'
 warehouse_size=  'Medium'
 auto_suspend = 300
 min_cluster_count = 1
 max_cluster_count = 3
 comment = 'This warehouse is to be used for evaluating assignment queries';
 
-use warehouse assignment_wh;
+USE warehouse assignment_wh;
 
-grant usage on warehouse assignment_wh to role admin;
-grant usage on warehouse assignment_wh to role developer;
-grant usage on warehouse assignment_wh to role pii;
+GRANT usage ON warehouse assignment_wh TO ROLE admin;
+GRANT usage ON warehouse assignment_wh TO ROLE developer;
+GRANT usage ON warehouse assignment_wh TO ROLE pii;
 
-create or replace database assignment_db;
+GRANT CREATE DATABASE ON ACCOUNT TO admin;
+USE ROLE admin;
+CREATE or REPLACE database assignment_db;
 
-grant usage on database assignment_db to role admin;
-grant usage on database assignment_db to role developer;
-grant usage on database assignment_db to role pii;
+GRANT usage ON database assignment_db TO ROLE admin;
+GRANT usage ON database assignment_db TO ROLE developer;
+GRANT usage ON database assignment_db TO ROLE pii;
 
-create or replace schema assignment_db.my_schema;
+CREATE or REPLACE schema assignment_db.my_schema;
 
-grant usage on schema assignment_db.my_schema to role admin;
-grant usage on schema assignment_db.my_schema to role developer;
-grant usage on schema assignment_db.my_schema to role pii;
+GRANT usage ON schema assignment_db.my_schema TO ROLE admin;
+GRANT usage ON schema assignment_db.my_schema TO ROLE developer;
+GRANT usage ON schema assignment_db.my_schema TO ROLE pii;
 
-Grant select on table assignment_db.my_schema.employees_ext to role developer;
-Grant select on table assignment_db.my_schema.employees_ext to role pii;
+GRANT SELECT ON TABLE assignment_db.my_schema.employees_ext TO ROLE developer;
+GRANT SELECT ON TABLE assignment_db.my_schema.employees_ext TO ROLE pii;
 
-create or replace table assignment_db.my_schema.employees_ext(
+CREATE OR REPLACE TABLE assignment_db.my_schema.employees_ext(
 id int,
 first_name string,
 last_name string,
 email string,
 location string,
-department string
+department STRING,
+elt_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+elt_by STRING DEFAULT 'AWS',
+filename STRING
 );
 
-create or replace file format assignment_db.my_schema.csv_format
+CREATE OR REPLACE FILE FORMAT assignment_db.my_schema.csv_format
 type = 'csv'
 field_delimiter = ','
 skip_header = 1;
 
-CREATE or replace STORAGE INTEGRATION s3_int
+CREATE OR REPLACE STORAGE INTEGRATION s3_int
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
@@ -69,23 +74,23 @@ CREATE or replace STORAGE INTEGRATION s3_int
 
 DESC INTEGRATION s3_int;
 
-create or replace stage csv_stage
+CREATE OR REPLACE stage csv_stage
 url = 's3://shishirsnowflakebucket/csv/'
-file_format = assignment_db.my_schema.csv_format
+FILE_FORMAT = assignment_db.my_schema.csv_format
 STORAGE_INTEGRATION = s3_int;
 
 List @csv_stage;
 desc stage csv_stage;
 
--- copy from external stage
-copy into assignment_db.my_schema.employees_ext
-from @csv_stage
+-- copy FROM external stage
+COPY INTO assignment_db.my_schema.employees_ext
+FROM @csv_stage
 files = ('employee_data_1.csv');
 
-select * from assignment_db.my_schema.employees_ext;
+SELECT * FROM assignment_db.my_schema.employees_ext;
 
 
-create or replace table assignment_db.my_schema.employees_int(
+CREATE OR REPLACE TABLE assignment_db.my_schema.employees_int(
 id int,
 first_name string,
 last_name string,
@@ -97,58 +102,58 @@ department string
 -- load file into internal stage
 -- put file://~/Desktop/employee_data_1.csv @int_stage;
 
-copy into assignment_db.my_schema.employees_int
-from @int_stage;
+COPY INTO assignment_db.my_schema.employees_int
+FROM @int_stage;
 
-select * from assignment_db.my_schema.employees_int;
+SELECT * FROM assignment_db.my_schema.employees_int;
 
-create or replace table assignment_db.my_schema.employees_var(
+CREATE OR REPLACE TABLE assignment_db.my_schema.employees_var(
 id int,
 data variant
 );
 
--- OBJECT_INSERT function to construct a JSON object for each row in the my_structured_table 
-insert into assignment_db.my_schema.employees_var
-select id, object_insert(object_insert(object_insert(object_insert(object_insert({}, 'first_name', first_name), 'last_name', last_name), 'email', email), 'location', location), 'department', department)
-from assignment_db.my_schema.employees_int;
+-- OBJECT_INSERT function to construct a JSON object fOR each row in the my_structured_TABLE 
+INSERT INTO assignment_db.my_schema.employees_var
+SELECT id, object_insert(object_insert(object_insert(object_insert(object_insert({}, 'first_name', first_name), 'last_name', last_name), 'email', email), 'location', location), 'department', department)
+FROM assignment_db.my_schema.employees_int;
 
-select * from assignment_db.my_schema.employees_var;
+SELECT * FROM assignment_db.my_schema.employees_var;
 
---create variant version of dataset
-create or replace file format parquet_format
+--CREATE variant version of dataset
+CREATE OR REPLACE FILE FORMAT parquet_format
 type = 'parquet';
 
-create or replace stage parquet_stage
-file_format = parquet_format;
+CREATE OR REPLACE stage parquet_stage
+FILE_FORMAT = parquet_format;
 
--- put file:///Users/shishirsingh/Desktop/userdata1.parquet @parquet_stage;
+-- put file:///users/shishirsingh/Desktop/userdata1.parquet @parquet_stage;
 
 -- inferschema of the parquet file
-select * from table(infer_schema(location=>'@parquet_stage',file_format=>'parquet_format'));
+SELECT * FROM TABLE(infer_schema(location=>'@parquet_stage',FILE_FORMAT=>'parquet_format'));
 
-select * from @parquet_stage;
+SELECT * FROM @parquet_stage;
 
-select $1:birthdate::string, $1:cc::string from @parquet_stage;
+SELECT $1:birthdate::string, $1:cc::string FROM @parquet_stage;
 
 
 
 -- implementing masking policy
-use role developer;
-use role pii;
+USE ROLE developer;
+USE ROLE pii;
 
-select * from assignment_db.my_schema.employees_ext;
+SELECT * FROM assignment_db.my_schema.employees_ext;
 
 -- creating masking policy
-create or replace masking policy email
-    as (val varchar) returns varchar -> 
-        case 
-        when current_role() in ('ACCOUNTADMIN', 'PII') then val
-        else '##-###-##'
-        end;
+CREATE OR REPLACE masking POLICY email
+    AS (val varchar) returns varchar -> 
+        CASE 
+        WHEN current_role() IN ('ACCOUNTADMIN', 'PII') THEN val
+        ELSE '##-###-##'
+        END;
 
 -- apply policy on a specific column
-alter table assignment_db.my_schema.employees_ext modify column email
-set masking policy email;
+ALTER TABLE assignment_db.my_schema.employees_ext MODIFY column email
+SET MASKING POLICY email;
 
 
 
